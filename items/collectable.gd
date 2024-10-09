62b148e4d853cc0f7ef9d77e996185a328552e4c
isaac wj a <1277521@sd61learn.ca>
2024-10-09 13:17:33 -0700

    Changed scoreboard a bit
    changed the scoreboard to fit and put the font on it
    ignore the import things

--- items/collectable.gd
+++ items/collectable.gd
@@ -6,36 +6,36 @@ extends Area2D
 var rewards_dict: Dictionary = {}
 
 func _ready() -> void:
-    parse_rewards()
+	parse_rewards()
 
 func _process(_delta):
-    position.y += dropspeed
+	position.y += dropspeed
 
 func _on_body_entered(_body):
-    var root = get_tree().root
-    root.get_node("Main/scoreboard").score += get_random_reward()
-    
-    var sound_instance = sound_scene.instantiate()
-    root.add_child(sound_instance)
-    sound_instance.play()
-    
-    queue_free()
+	var root = get_tree().root
+	root.get_node("Main/scoreboard").score += get_random_reward()
+	
+	var sound_instance = sound_scene.instantiate()
+	root.add_child(sound_instance)
+	sound_instance.play()
+	
+	queue_free()
 
 func parse_rewards() -> void:
-    for line in rewards.split("\n"):
-        var parts = line.split(":")
-        if parts.size() == 2:
-            var key = parts[0].strip_edges()
-            var value = parts[1].strip_edges().to_float()
-            rewards_dict[int(key)] = float(value)
+	for line in rewards.split("\n"):
+		var parts = line.split(":")
+		if parts.size() == 2:
+			var key = parts[0].strip_edges()
+			var value = parts[1].strip_edges().to_float()
+			rewards_dict[int(key)] = float(value)
 
 func get_random_reward():
-    var random_value = randf()
-    var cumulative_probability = 0.0
+	var random_value = randf()
+	var cumulative_probability = 0.0
 
-    for reward in rewards_dict.keys():
-        cumulative_probability += rewards_dict[reward]
-        if random_value < cumulative_probability:
-            return reward
+	for reward in rewards_dict.keys():
+		cumulative_probability += rewards_dict[reward]
+		if random_value < cumulative_probability:
+			return reward
 
-    return 1
+	return 1
--- scoreboard/scoreboard.gd
+++ scoreboard/scoreboard.gd
@@ -4,9 +4,9 @@ var score = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$scoretext.text = "Your Score: " + str(score)
+	$scoretext.text = "Score: " + str(score)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
-	$scoretext.text = "Your Score: " + str(score)
+	$scoretext.text = "Score: " + str(score)
